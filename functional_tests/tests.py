e988301d3464e7ced5f8e845da6d4c961d91b6b0
elise <elise.ajay@team.telstra.com>
2018-09-07 21:45:15 +1000

    new url, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -49,7 +49,7 @@ class NewVisitorTest(LiveServerTestCase):
         # (Edith's hobby is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
 
-        # When she hits enter, the page updates, and now the page 
+        # When she hits enter, the page updates, and  the page 
         # lists "1: Buy peacock feathers" as an item in a to-do 
         # list table
         inputbox.send_keys(Keys.ENTER)
@@ -102,7 +102,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_now_in_list_table('1: Buy Milk')
+        self.wait_for_row_in_list_table('1: Buy Milk')
 
         # Francis gets his own unique URL
         francis_list_url = self.browser.current_url
